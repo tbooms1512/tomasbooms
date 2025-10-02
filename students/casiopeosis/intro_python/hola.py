f56580b3b48974aa443a7dcb1ef9bd66775787b8
casinopea <dreincognito@dr.local>
2025-10-01 18:19:17 -0600

    Intro Python: notas.txt, entrega.py, Dockerfile y README

---
+++ students/casiopeosis/intro_python/hola.py
@@ -0,0 +1,8 @@
+import math
+print("1. DS2 doesn't deserve the hate it gets.")
+i = math.sqrt(2819377821.128937)
+print("2. You owe me: $", i)
+j= input("What is your name?: ")
+def longitud(palabra):
+	return len(j)
+print("Your name is ", longitud(j), " characters long")
