55e1ba25a241954e58069b83712e8cd08c2a4711
lulugtz11 <lulugutierrez32landa@gmail.com>
2025-10-05 00:44:49 -0600

    Intro Python: notas.txt, entrega.py, Dockerfile y README

---
+++ students/lulugtz11/intro_python/hola.py
@@ -0,0 +1,7 @@
+def doble(n):
+    return 2*n
+
+print("Hola desde hola.py")           # 1) saludo
+x = 6*7                               # 2) operación aritmética
+print("x =", x)                       #    y lo mostramos
+print("doble(5) =", doble(5))         # 3) llamamos a la función y mostramos
