3b25382d54c247e25b36129f3fd8e8576f206991
natquintanag <guzman60@gmail.com>
2025-10-06 21:21:40 -0600

    Tarea intro python

---
+++ students/natquintanag/intro_python/hola.py
@@ -0,0 +1,15 @@
+# hola.py
+
+# Un saludo
+print("Hola! Este es mi primer script en Python")
+
+# ⃣Una variable con una operación aritmética
+suma = 7 + 5
+print("El resultado de 7 + 5 es:", suma)
+
+# ️Una función simple
+def cuadrado(x):
+    return x * x
+
+print("El cuadrado de 4 es:", cuadrado(4))
+
