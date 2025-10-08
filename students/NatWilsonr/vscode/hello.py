3f99e289787375e4e6fd59fc89ea2a594987a620
Nat Wilson <nwilsonr@itam.mx>
2025-10-07 22:16:58 -0600

    Agregué bitácora y ejercicio de hello.py para prueba de VSCode

---
+++ students/NatWilsonr/vscode/hello.py
@@ -0,0 +1,14 @@
+def say_hello(name):
+    print(f"Hello,{name}!")
+
+
+def say_hello(name):
+    print(f"Hello, {name}!")
+    x = 3 + "a"
+
+
+def add_numbers(a: int, b: int) -> int:
+    return a + b
+
+
+add_numbers("3", 4)
